1ae99c0957e5bcf9ae42b722ed69eb39bc886d6c
Max <st_maxwell_@mail.ru>
2016-02-09 08:19:09 +0200

    05-01

---
+++ src/main/java/com/netcracker/lab1/stetsenko/controller/Actions.java
@@ -0,0 +1,19 @@
+package com.netcracker.lab1.stetsenko.controller;
+
+/**
+ * Created by Макс on 25.01.2016.
+ */
+public enum Actions {
+    STARTPAGE1,
+    STARTPAGE2,
+    TASKLIST1,
+    TASKLIST2,
+    TASKLIST3,
+    TASKLIST4,
+    ADDTASK1,
+    ADDTASK2,
+    ADDTASK3,
+    EDITTASK1,
+    EDITTASK2
+
+}
